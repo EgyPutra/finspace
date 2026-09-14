19b8defbe40fb7c7bdd2ac66513868704fb4661b
EgyPutra12 <egyputra1225@gmail.com>
2026-09-14 19:51:56 +0800

    Reduce mobile receipt payload size

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'finspace-shell-v12';
+const CACHE_NAME = 'finspace-shell-v13';
 const APP_SHELL = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.webmanifest', '/assets/icon.svg'];
 
 self.addEventListener('install', (event) => {
